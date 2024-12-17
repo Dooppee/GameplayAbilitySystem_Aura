cfb3694272c9ed2199dd104502884a02df344afa
Dooppee <101252650+Dooppee@users.noreply.github.com>
2024-12-17 17:09:21 +0800

    12M17D02

---
+++ Source/Aura/Private/Character/EnemyCharacter.cpp
@@ -0,0 +1,5 @@
+// Dop Practice Project.
+
+
+#include "Character/EnemyCharacter.h"
+
---
+++ Source/Aura/Public/Character/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Dop Practice Project.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
