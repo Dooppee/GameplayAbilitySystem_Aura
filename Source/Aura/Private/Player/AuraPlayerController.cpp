2fd7ef36d3c0593221714f08ebc41a66ff6c997f
Dooppee <101252650+Dooppee@users.noreply.github.com>
2024-12-23 18:02:14 +0800

    12M23D02

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,39 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+	//玩家子系统;
+	UEnhancedInputLocalPlayerSubsystem* Subsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	//玩家子系统添加映射
+	Subsystem->AddMappingContext(AuraContext,0);
+	//设置鼠标状态
+	bShowMouseCursor = true;
+	//箭头种类
+	DefaultMouseCursor = EMouseCursor::Type::Default;
+	//设置光标，不要锁定再视口
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+}
